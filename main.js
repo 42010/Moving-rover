c765c77c2ef8d7b76c04b5286b0dd746d6b609e7
42010 <72303709+42010@users.noreply.github.com>
2020-12-11 16:49:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,86 +1,90 @@
 canvas = document.getElementById("myCanvas");
 ctx = canvas.getContext("2d")
-var background_image = "https://i.postimg.cc/KzWwHPcq/racing.gif";
-var car1_image = "th.jpeg";
-var car1_x = 10;
-var car1_y = 10;
-var car1_width = 70;
-var car1_height = 70;
-var car2_image = "th.jpeg";
-var car2_x = 10;
-var car2_y = 10;
-var car2_width = 70;
-var car2_height = 70;
+var mars = ["mars.jpg", "th1.jpeg", "th2.jpg"];
+var random = Math.floor(Math.random()*3);
+var background_image = mars[random];
+var rover_image = "rover.png";
+var rover_x = 10;
+var rover_y = 10;
+var rover_width = 70;
+var rover_height = 70;
 
 
-function add(){
+function add() {
     background_imgTag = new Image();
     background_imgTag.onload = uploadBackground();
     background_imgTag.src = background_image;
 
-    car1_imgTag = new Image();
-    car1_imgTag.onload = uploadCar1();
-    car1_imgTag.src = car1_image;
-    
-    car2_imgTag = new Image();
-    car2_imgTag.onload = uploadCar2();
-    car2_imgTag.src = car2_image
+    rover_imgTag = new Image();
+    rover_imgTag.onload = uploadRover();
+    rover_imgTag.src = rover_image;
+
 }
 
 function uploadBackground() {
     ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
 }
 
-function uploadCar1(){
-    ctx.drawImage(car1_imgTag, car1_x, car1_y, car1_width, car1_height);
+function uploadRover() {
+    ctx.drawImage(rover_imgTag, rover_x, rover_y, rover_width, rover_height);
 }
 
-function uploadCar2(){
-    ctx.drawImage(car2_imgTag, car2_x, car2_y, car2_width, car2_height);
-}
+window.addEventListener("keydown", my_keydown);
 
-windows.addEventListener("keydown", my_keydown);
-
-function my_keydown(e){
-    keypressed = e.KeyCode;
+function my_keydown(e) {
+    keyPressed = e.KeyCode;
     console.log(keyPressed);
-    if(keyPressed == "38"){
+    if (keyPressed == "38") {
         up();
         console.log("up");
     }
-    if(keyPressed == "40"){
+    if (keyPressed == "40") {
         down();
         console.log(keyPressed);
     }
-    if(keyPressed == "37"){
+    if (keyPressed == "37") {
         left();
         console.log(keyPressed)
     }
-    if(keyPressed == "39"){
+    if (keyPressed == "39") {
         right();
         console.log(keyPressed);
     }
 }
 
-windows.addEventListener("keydown", my_keydown1)
+function up() {
+    if (rover_y >= 0) {
+        rover_y -= 10;
+        console.log("up Arrow =" + rover_x + "-" + rover_y);
+        uploadBackground();
+        uploadRover();
+    }
+}
 
-function my_keydown1(e){
-    keypressed = e.keyCode;
-    console.log(keyPressed);
-    if(keyPressed == "87"){
-        up();
-        console.log("up")
+function down() {
+    if (rover_y <= 750) {
+
+        rover_y += 10;
+        console.log("down Arrow =" + rover_x + "-" + rover_y);
+        uploadBackground();
+        uploadRover();
     }
-    if(keyPressed == "65"){
-        left();
-        console.log("left")
+}
+
+function left() {
+    if (rover_x >= 0) {
+        rover_x -= 10;
+        console.log("left Arrow =" + rover_x + "-" + rover_y);
+        uploadBackground();
+        uploadRover();
     }
-    if(keyPressed == "83"){
-        down();
-        console.log("down");
+}
+
+function right() {
+    if (rover_x <= 550) {
+        rover_x += 10;
+        console.log("right Arrow =" + rover_x + "-" + rover_y);
+        uploadBackground();
+        uploadRover();
     }
-    if(keyPressed == "68"){
-        right();
-        console.log("right")
-    }   
 }
